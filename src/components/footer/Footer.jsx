209a391eb194a08af90108c0eec16477c6bbc537
Sreehari B S <isreeharibs@gmail.com>
2025-01-26 07:57:22 +0530

    certified logo in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import "./footer.css";
 import NLClogo from "../../assets/NLC logo footer.png";
+import Certified from "../../assets/certified.png"
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -16,19 +17,20 @@ const Footer = () => {
     >
       <div className="footerGradient">
         <div className="flex flex-col space-y-2">
-          <div>
-            <img src={NLClogo} alt="NLC_logo" className="w-[128px] h-[84px]" />
+          <div className="flex flex-row justify-center items-center">
+            <img src={NLClogo} alt="NLC_logo" className="w-[128px] h-[84px] mr-16 footerLogo" />
+            <img src={Certified} alt="NLC_Cirtified" className="w-[274px] h-[130px] footerCertificate" />
           </div>
         </div>
         <div className="flex flex-col space-y-4">
           <Link to="/privacy_policy">
-            <div className="plus-jakarta cursor-pointer tracking-wide hover:font-semibold">
+            <div className="plus-jakarta cursor-pointer tracking-wide hover:font-semibold text-center">
               {t("privacyPolicy")}
             </div>
           </Link>
 
           <Link to="/terms_and_conditions">
-            <div className="plus-jakarta cursor-pointer tracking-wide hover:font-semibold">
+            <div className="plus-jakarta cursor-pointer tracking-wide hover:font-semibold text-center">
               Terms & Conditions
             </div>
           </Link>
@@ -36,7 +38,7 @@ const Footer = () => {
 
         <div className="footerLine"></div>
 
-        <div className="flex flex-col space-y-4">
+        <div className="flex flex-col space-y-4 items-center">
           <div className="plus-jakarta tracking-wide">
             <div>+91 88486 06589</div>
             <div>+91 89219 03274</div>
